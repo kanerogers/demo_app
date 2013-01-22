5e03b273768880ea62454e5986c964298e938fda
Kane Rogers <kane.rogers@bluereef.com.au>
2013-01-22 20:27:51 +1100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '368f7ebb67b0f7fee8233b337d6104a1892042ca88346cf2cd4cc208e7db48c3e937ac0e4ba872c008637992c86cf2b495084398ea883af03d9dee5170dcc823'
